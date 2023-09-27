e35c6bf2f7dc912149853bf1ffb5f1d62340836a
Santiago Jaimes Pinilla <sjaimes@uniempresarial.edu.co>
2023-09-27 02:30:17 -0500

    BD table usuario

--- bd_park.sql
+++ bd_park.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-09-2023 a las 06:45:05
+-- Tiempo de generación: 27-09-2023 a las 09:29:51
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
@@ -31,20 +31,24 @@ CREATE TABLE `usuario` (
   `usu_id` int(11) NOT NULL,
   `usu_nombre` varchar(100) NOT NULL,
   `usu_apellido` varchar(100) NOT NULL,
+  `usu_edad` int(10) NOT NULL,
   `usu_cedula` varchar(70) NOT NULL,
   `usu_mail` varchar(150) NOT NULL,
   `usu_usuario` varchar(50) NOT NULL,
   `usu_password` varchar(150) NOT NULL,
   `usu_identifier` varchar(255) NOT NULL,
-  `usu_key` varchar(255) NOT NULL
+  `usu_key` varchar(255) NOT NULL,
+  `usu_estado` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`usu_id`, `usu_nombre`, `usu_apellido`, `usu_cedula`, `usu_mail`, `usu_usuario`, `usu_password`, `usu_identifier`, `usu_key`) VALUES
-(1, 'Santiago', 'Jaimes Pinilla', '1014976535', 'jaisan@gmail.com', 'jaisan04', '827ccb0eea8a706c4c34a16891f84e7b', 'asd1asdaserwtopasdM.Gm46Gutx/Sofv2bVD7h1', 'ERT1ERTaserwtopERT0rfzd16xr7098x10Na60i0');
+INSERT INTO `usuario` (`usu_id`, `usu_nombre`, `usu_apellido`, `usu_edad`, `usu_cedula`, `usu_mail`, `usu_usuario`, `usu_password`, `usu_identifier`, `usu_key`, `usu_estado`) VALUES
+(1, 'Santiago', 'Jaimes Pinilla', 19, '1014976535', 'jaisan@gmail.com', 'jaisan', '827ccb0eea8a706c4c34a16891f84e7b', 'asd1asdaserwtopasdvG6nTraFs7Iw8CG9vQtdU0', 'ERT1ERTaserwtopERT5PxOu9NRw98FxcigypHZB.', 1),
+(2, 'Jose Felipe', 'Carvajal Campos', 88, '1000240193', 'elgaymasgrande@gmail.com', 'soyhomosexual', 'e10adc3949ba59abbe56e057f20f883e', 'asd1asdaserwtopasddp3mpw/PE.1Ljk.yTV83J1', 'ERT1ERTaserwtopERTcuqfOBxECmFDVLD.jrMgO0', 1),
+(3, 'Sebatian', 'Ballen Loaiza', 20, '1001200029', 'mejoramigodesof@gmail.com', 'teamsofiaforever', '25f9e794323b453885f5181f1b624d0b', 'asd1asdaserwtopasdqanMfRcOmWGaOGX0rFjtQ1', 'ERT1ERTaserwtopERTaxc56Tz5fcQXhHFe/Wx9i1', 1);
 
 --
 -- Índices para tablas volcadas
@@ -64,7 +68,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `usu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `usu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
